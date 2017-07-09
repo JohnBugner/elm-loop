8891b71784060441a6e070b79b2ed5e1e21dc92a
John Bugner <john.bugner@gmail.com>
2017-07-08 21:44:29 -0500

    Added documentation comments.

--- src/Loop.elm
+++ src/Loop.elm
@@ -1,10 +1,24 @@
 module Loop exposing (while, until)
 
-while : (a -> Bool) -> (a -> a) -> a -> a
-while p u s =
-    if p s
-    then while p u (u s)
-    else s
+{-| Repeatedly apply a function to a value while or until a predicate holds.
 
+# Looping
+@docs while, until
+-}
+
+{-| Repeatedly apply a function to a value *while* a predicate holds.
+
+    while (\ n -> n < 10) ((+) 5) 2 == 12
+-}
+while : (a -> Bool) -> (a -> a) -> a -> a
+while p f v =
+    if p v
+    then while p f (f v)
+    else v
+
+{-| Repeatedly apply a function to a value *until* a predicate holds.
+
+    until (\ n -> n > 10) ((+) 5) 2 == 12
+-}
 until : (a -> Bool) -> (a -> a) -> a -> a
 until p = while (not << p)
